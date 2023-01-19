0ed6a191f88d84b1c7aa6c94149a06d8200f1341
MohammadHosseini <sayedmh50@gmail.com>
2023-01-19 18:02:39 +0330

    change in train file and add some sile and folder

---
+++ train.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 17:43:16 2023
+
+@author: mohammad
+"""
+
+from src.graphdata.heterodata import CurriculumDataset
+
+
+a=CurriculumDataset('/data')
